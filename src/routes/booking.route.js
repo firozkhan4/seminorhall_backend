9a0651dbc0e69573b4ffbb5b8104629177acf67f
Firoz Khan <firozkhan907979@gmail.com>
2026-05-04 13:40:19 +0530

    fix

--- src/routes/booking.route.js
+++ src/routes/booking.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import bookingModel from "../models/booking.model.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/", authMiddleware, async (req, res) => {
   const filters = {};
   if (req.query.start) {
     filters.start_time = {
@@ -19,25 +20,35 @@ router.get("/", async (req, res) => {
   return res.status(200).json({ output: booking });
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", authMiddleware, async (req, res) => {
   const { id } = req.params;
   const booking = await bookingModel.findById(id);
   return res.status(200).json(booking);
 });
 
-router.post("/", async (req, res) => {
-  const { user_id, hall_id, title, description, start_time, end_time } =
-    req.body;
+router.post("/", authMiddleware, async (req, res) => {
+  const { hall_id, title, description, start_time, end_time } = req.body;
 
   try {
     const booking = await bookingModel.create({
-      user_id,
+      user_id: req.user.id,
       hall_id,
       title,
       description,
       start_time,
       end_time,
     });
+    return res.status(200).json({ output: booking });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+router.patch("/:id/status", authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+  try {
+    const booking = await bookingModel.findByIdAndUpdate(id, { status });
     return res.status(200).json({ output: booking });
   } catch (error) {
     return res.status(400).json({ error: error.message });
